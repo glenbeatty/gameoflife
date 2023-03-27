a83287ded09ce62dc448791cc862d4f97fbf3c56
glenbeatty <46807478+glenbeatty@users.noreply.github.com>
2023-03-27 16:09:02 -0700

    Added Gosper Glider Gun

--- presets.js
+++ presets.js
@@ -27,4 +27,41 @@ function preset_classDemo(){
     activeCells.push(new Cell(10,17));
 }
 
-
+function preset_gosperGliderGun(){
+    activeCells.push(new Cell(3,26));
+    activeCells.push(new Cell(4,24));
+    activeCells.push(new Cell(4,26));
+    activeCells.push(new Cell(5,14));
+    activeCells.push(new Cell(5,15));
+    activeCells.push(new Cell(5,22));
+    activeCells.push(new Cell(5,23));
+    activeCells.push(new Cell(5,36));
+    activeCells.push(new Cell(5,37));
+    activeCells.push(new Cell(6,13));
+    activeCells.push(new Cell(6,17));
+    activeCells.push(new Cell(6,22));
+    activeCells.push(new Cell(6,23));
+    activeCells.push(new Cell(6,36));
+    activeCells.push(new Cell(6,37));
+    activeCells.push(new Cell(7,2));
+    activeCells.push(new Cell(7,3));
+    activeCells.push(new Cell(7,12));
+    activeCells.push(new Cell(7,18));
+    activeCells.push(new Cell(7,22));
+    activeCells.push(new Cell(7,23));
+    activeCells.push(new Cell(8,2));
+    activeCells.push(new Cell(8,3));
+    activeCells.push(new Cell(8,12));
+    activeCells.push(new Cell(8,16));
+    activeCells.push(new Cell(8,18));
+    activeCells.push(new Cell(8,19));
+    activeCells.push(new Cell(8,24));
+    activeCells.push(new Cell(8,26));
+    activeCells.push(new Cell(9,12));
+    activeCells.push(new Cell(9,18));
+    activeCells.push(new Cell(9,26));
+    activeCells.push(new Cell(10,13));
+    activeCells.push(new Cell(10,17));
+    activeCells.push(new Cell(11,14));
+    activeCells.push(new Cell(11,15));
+}
